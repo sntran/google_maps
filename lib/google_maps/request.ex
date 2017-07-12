c02984af6177ac000fd9846077ba7da725200c68
Son Tran-Nguyen <code@sntran.com>
2017-07-12 13:44:35 -0500

    Resolve #2 supporting key per request
    As all functions basically merge arguments into a single keyword list
    for `get/2`, we can support a `key` argument that will override the one
    in configuration. Therefore, to use a separate key for a certain call,
    set it in the `options`.
    
    ## Example
    
    ```elixir
    GoogleMaps.directions("Toronto", "Montreal", key: API_KEY_1)
    ```

--- lib/google_maps/request.ex
+++ lib/google_maps/request.ex
@@ -10,14 +10,17 @@ defmodule GoogleMaps.Request do
   """
   @spec get(String.t, keyword()) :: GoogleMaps.Response.t
   def get(endpoint, params) do
-    params = Enum.map(params, &transform_param/1)
+    params =
+      [key: @api_key]
+      |> Keyword.merge(params)
+      |> Enum.map(&transform_param/1)
     get("#{endpoint}?#{URI.encode_query(params)}")
   end
 
   # HTTPoison callbacks.
   def process_url(url) do
     %{path: path, query: query} = URI.parse(url)
-    "https://maps.googleapis.com/maps/api/#{path}/json?key=#{@api_key}&#{query}"
+    "https://maps.googleapis.com/maps/api/#{path}/json?#{query}"
   end
 
   def process_response_body(body) do
