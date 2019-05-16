9a638ed6cee1c3b1575d4efc5aaba5edc2da4f43
Rene Klein <rk@fleetlink.de>
2019-05-16 10:52:57 +0200

    delete config comment

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,3 @@ use Mix.Config
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
-
-# config :google_maps,
-# url: "https://maps.googleapis.com/maps/api/",
-# api_key: api_key
